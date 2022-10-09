42fc2975faaf57197d2c0a832dfed992b1d01ea7
20A91A04D3 <84578673+20A91A04D3@users.noreply.github.com>
2022-10-09 14:25:41 +0530

    def rev(n):     s=0     l=len(str(n))     while n:         a=n%10         s=s+(a*10**(l-1))         l-=1         n=n//10     return s n=int(input()) r=rev(n) a=n*n b=r*r if a==rev(b):     print('True') else:     print('False')

---
+++ Adam_number.py
@@ -0,0 +1,17 @@
+def rev(n):
+    s=0
+    l=len(str(n))
+    while n:
+        a=n%10
+        s=s+(a*10**(l-1))
+        l-=1
+        n=n//10
+    return s
+n=int(input())
+r=rev(n)
+a=n*n
+b=r*r
+if a==rev(b):
+    print('True')
+else:
+    print('False')
